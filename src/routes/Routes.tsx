dfe248e7c4ae4ead44954eede4da10b3dd8f081a
Roberto Carvalho <robertocarvalho508@gmail.com>
2023-03-30 00:23:31 -0300

    adicionei area de login

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,15 @@
 /* eslint-disable indent */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login';
 import Photos from '../pages/photos';
 
 const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+
         <Route path="/photos" element={<Photos />} />
       </Routes>
     </BrowserRouter>
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,5 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
-const routes = [{ label: 'photos', url: '/photos' }];
+const routes = [
+  { label: 'photos', url: '/photos' },
+  { label: 'login', url: '/login' }
+];
 
 export default routes;
